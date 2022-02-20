22c5151c18f4b7518a7f93877b3940d322875d62
livingstone23 <livingstone23@gmail.com>
2022-02-20 13:35:32 +0100

    Initial Charge

---
+++ HNL.CreditManager/Manager.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Manager.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manager.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext  
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ HNL.CreditManager/Manager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Manager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
