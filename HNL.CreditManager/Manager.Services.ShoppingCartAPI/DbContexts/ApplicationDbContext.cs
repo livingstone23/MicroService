800dba2ee079b66497b3e76cdd959f80c3b5b5f1
livingstone23 <livingstone23@gmail.com>
2022-04-17 19:51:55 +0200

    Integrating Shopping Cart API

---
+++ HNL.CreditManager/Manager.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Manager.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manager.Services.ShoppingCartAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext  
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+
+
+        
+
+
+    }
+}
---
+++ HNL.CreditManager/Manager.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Manager.Services.ShoppingCartAPI.Models;
+using Manager.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Manager.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                config.CreateMap<Cart, CartDto>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
